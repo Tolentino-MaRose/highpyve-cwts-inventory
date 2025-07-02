9ddf46cff0ae88b4d961ae858f27e9a84cfd6ddd
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-07-02 13:30:42 +0800

    add todo list in routes

---
+++ routes/logs.py
@@ -0,0 +1,9 @@
+# TODO DAY 2 - Tolentino:
+# - Create Blueprint: logs_bp
+# - Define routes for /logs
+
+# TODO DAY 2 - Ocariza:
+# - Implement CRUD routes: Add Log (IN/OUT), View, Delete
+
+# TODO DAY 3 - Ocariza:
+# - Add error handling and input validation for log endpoints
